7fc5e46e8bb6ea1826ca522f0686bd533e0cc7fb
shahrodkh <shahrodkh@gmail.com>
2017-02-10 13:18:00 -0800

    adding Bita's working demo to repo

---
+++ scratch-canvas-master/app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const socketio = require('socket.io')
+
+let server = http.createServer(app);
+let io = socketio.listen(server);
+server.listen(8080);
+app.use(express.static(__dirname + '/public'));
+console.log("Server running on http://localhost:8080");
+
+
+///////////////////////////////////////////
+
+// array of all lines drawn
+var line_history = [];
+
+// event-handler for new incoming connections
+io.on('connection', function (socket) {
+
+   // send drawing history to the new client
+   for (var i in line_history) {
+      socket.emit('draw_line', { line: line_history[i] } );
+   }
+
+   // add handler for message type "draw_line".
+   socket.on('draw_line', function (data) {
+      // add received line to history 
+      line_history.push(data.line);
+      // send line to all clients
+      io.emit('draw_line', { line: data.line });
+   });
+});
