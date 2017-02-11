0c8e98e4da444917f55130f1d41369dfaf68c1ae
Bita Djaghouri <bita@bitadj.com>
2017-02-10 19:46:19 -0800

    fixed all spacing issues

--- public/client.js
+++ public/client.js
@@ -1,54 +1,54 @@
 document.addEventListener("DOMContentLoaded", () => {
-   let mouse = {
-      click: false,
-      move: false,
-      pos: {x:0, y:0},
-      pos_prev: false
-   };
+  let mouse = {
+    click: false,
+    move: false,
+    pos: { x: 0, y: 0 },
+    pos_prev: false
+  };
 
-   // get canvas element and create context
-   let canvas  = document.getElementById('drawing');
-   let context = canvas.getContext('2d');
-   const width   = canvas.width;
-   const height  = canvas.height;
-   let socket  = io.connect();
+  // get canvas element and create context
+  let canvas = document.getElementById('drawing');
+  let context = canvas.getContext('2d');
+  const width = canvas.width;
+  const height = canvas.height;
+  let socket = io.connect();
 
-   // register mouse event handlers
-   canvas.onmousedown = (e) => { mouse.click = true; };
-   canvas.onmouseup = (e) => { mouse.click = false; };
+  // register mouse event handlers
+  canvas.onmousedown = (e) => { mouse.click = true; };
+  canvas.onmouseup = (e) => { mouse.click = false; };
 
-   canvas.onmousemove = (e) => {
-      mouse.pos.x = e.clientX;
-      mouse.pos.y = e.clientY;
-      mouse.move = true;
-   };
+  canvas.onmousemove = (e) => {
+    mouse.pos.x = e.clientX;
+    mouse.pos.y = e.clientY;
+    mouse.move = true;
+  };
 
-   // draw line received from server
-   socket.on('draw_line', (data) => {
-      let line = data.line;
-      context.beginPath();
+  // draw line received from server
+  socket.on('draw_line', (data) => {
+    let line = data.line;
+    context.beginPath();
 
-      //  set line width
-      context.lineWidth=10;
-      // set end cap of line 'round' 'square' 'butt'
-      context.lineCap='round';
+    //  set line width
+    context.lineWidth = 10;
+    // set end cap of line 'round' 'square' 'butt'
+    context.lineCap = 'round';
 
-      context.moveTo(line[0].x, line[0].y);
-      context.lineTo(line[1].x, line[1].y);
-      context.strokeStyle="#FF0000";
-      context.stroke();
-   });
-   
-   // main loop, running every 5ms
-   function mainLoop() {
-      // check if the user is drawing
-      if (mouse.click && mouse.move && mouse.pos_prev) {
-         // send line to to the server
-         socket.emit('draw_line', { line: [ mouse.pos, mouse.pos_prev ] });
-         mouse.move = false;
-      }
-      mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
-      setTimeout(mainLoop, 5);
-   }
-   mainLoop();
+    context.moveTo(line[0].x, line[0].y);
+    context.lineTo(line[1].x, line[1].y);
+    context.strokeStyle = "#FF0000";
+    context.stroke();
+  });
+
+  // main loop, running every 5ms
+  function mainLoop() {
+    // check if the user is drawing
+    if (mouse.click && mouse.move && mouse.pos_prev) {
+      // send line to to the server
+      socket.emit('draw_line', { line: [mouse.pos, mouse.pos_prev] });
+      mouse.move = false;
+    }
+    mouse.pos_prev = { x: mouse.pos.x, y: mouse.pos.y };
+    setTimeout(mainLoop, 5);
+  }
+  mainLoop();
 });
